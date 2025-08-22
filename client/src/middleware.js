fcc811fdf9d5e5d97101183ff41afe82e9735e94
hassanhanif72 <hassanhanif72@gmail.com>
2025-08-23 02:55:47 +0500

    Middleware Changes

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -19,6 +19,7 @@ export function middleware(request) {
 
 export const config = {
     matcher: [
-        "/((?!_next/static|_next/image|favicon.ico).*)",
+        "/((?!_next/static|_next/image|favicon.ico).*)", // global matcher
+        // "/",  // private matcher
     ],
 };
